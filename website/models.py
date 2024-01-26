6a9ed9d964e4a0fe119ddcadeb7ef39aa91fbb4c
andreaBalaba <andreabalaba0827@gmail.com>
2024-01-26 19:25:15 +0800

    create ballot and edit ballot backend
    edit ballot have close/open ballot, delete ballot, add candidates and delete candidates

--- website/models.py
+++ website/models.py
@@ -6,6 +6,7 @@ class User(db.Model, UserMixin):
     userId = db.Column(db.String(15), primary_key=True)
     password = db.Column(db.String(100), nullable=False)
     userType = db.Column(db.String(20), nullable=False)
+    ballots = db.relationship('Ballot', backref='user', lazy=True)
 
     def get_id(self):
         return str(self.userId)
@@ -26,14 +27,16 @@ class Candidate(db.Model):
     studentId = db.Column(db.String(15), db.ForeignKey('student.studentId'), nullable=False, unique=True)
     position = db.Column(db.String(100), nullable=False)
     voteCount = db.Column(db.Integer)
+    ballot_id = db.Column(db.Integer, db.ForeignKey('ballot.id'), nullable=False)
     posts = db.relationship('Post')
+    student = db.relationship('Student', backref='candidate', lazy=True)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(10000))
     image = db.Column(db.LargeBinary)
     studentId = db.Column(db.String(15), db.ForeignKey('candidate.studentId'), nullable=False)
-    
+
 class Vote(db.Model):
     voter = db.Column(db.String(15), db.ForeignKey('student.studentId'), primary_key=True, nullable=False)
     president = db.Column(db.String(15), db.ForeignKey('student.studentId'))
@@ -52,3 +55,14 @@ class Vote(db.Model):
 class BallotStatus(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     isOpen = db.Column(db.Boolean, nullable=False)
+    ballots = db.relationship('Ballot', back_populates='ballot_status', lazy=True)
+
+class Ballot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(15), db.ForeignKey('user.userId'), nullable=False)
+    ballot_status_id = db.Column(db.Integer, db.ForeignKey('ballot_status.id'), nullable=False)
+    ballot_name = db.Column(db.String(100), nullable=False)
+    candidates = db.relationship('Candidate', backref='ballots', lazy=True)
+    ballot_status = db.relationship('BallotStatus', back_populates='ballots')
+
+
--- website/static/index.js
+++ website/static/index.js
@@ -2,49 +2,23 @@ function redirectTo(route) {
   window.location.href = route;
 }
 
-//Create Ballot JavaScript
-function fetchStudentInfo(studentId, position, index) {
-  const nameElement = document.getElementById(`${position}_name_${index}`);
-  const courseElement = document.getElementById(`${position}_course_${index}`);
-
-  if (!studentId) {
-    nameElement.innerText = '';
-    courseElement.innerText = '';
-    return;
-  }
-
-  fetch(`/get_student_info/${studentId}`)
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        nameElement.innerText = `Name: ${data.name}`;
-        courseElement.innerText = `Course: ${data.course}`;
-      } 
-      else {
-        nameElement.innerText = '';
-        courseElement.innerText = '';
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching student info:', error);
-    });
-}
-
+// Create & Edit Ballot JavaScript
 document.addEventListener('DOMContentLoaded', function () {
+
   const positions = [
     'president', 'vice_president', 'executive_board_sec', 'vp_finance',
     'vp_academic_affairs', 'vp_internal_external_affairs', 'vp_public_relations',
     'vp_research_dev', 'first_yr_rep', 'second_yr_rep', 'third_yr_rep', 'fourth_yr_rep'
   ];
 
-  let lastFocusedInput = null; 
+  let lastFocusedInput = null;
 
   positions.forEach(position => {
     for (let i = 1; i <= 4; i++) {
       const inputElement = document.getElementById(`${position}_id${i}`);
       if (inputElement) {
         inputElement.addEventListener('focus', function () {
-          lastFocusedInput = this; 
+          lastFocusedInput = this;
         });
 
         inputElement.addEventListener('blur', function () {
@@ -59,46 +33,157 @@ document.addEventListener('DOMContentLoaded', function () {
           checkForDuplicates(studentId, positionName, index);
 
           fetchStudentInfo(studentId, positionName, index);
+          
         });
       }
     }
   });
 
+  fetch('/get-ballot-status')
+    .then(response => response.json())
+    .then(data => {
+
+      if (data.success) {
+        const isOpen = data.isOpen;
+
+        const statusElement = document.getElementById('ballot-status');
+
+        if (isOpen) {
+          statusElement.innerText = 'Voting is now open.';
+        } else {
+          statusElement.innerText = 'Voting is now closed.';
+        }
+      } else {
+        console.error('Failed to fetch ballot status');
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching ballot status:', error);
+    });
+
+    const studentIdInput = document.getElementById('studentId');
+  const nameElement = document.getElementById('candidateName');
+  const courseElement = document.getElementById('candidateCourse');
+
+  studentIdInput.addEventListener('blur', async function () {
+    const studentId = this.value;
+
+    if (studentId) {
+      try {
+        const response = await fetch(`/check_existing_candidate/${studentId}`);
+        const data = await response.json();
+
+        if (data.exists) {
+          alert(`Student ID ${studentId} is already a candidate. Please enter a different student ID.`);
+        } else {
+
+          fetch(`/get_student_info/${studentId}`)
+            .then(response => response.json())
+            .then(data => {
+              if (data.success) {
+                const studentData = data.data;
+                nameElement.innerText = `Name: ${studentData.name.first} ${studentData.name.last}`;
+                courseElement.innerText = `Course: ${studentData.course}`;
+              } else {
+                nameElement.innerText = '';
+                courseElement.innerText = '';
+                alert(`Error: ${data.message}`);
+              }
+            })
+            .catch(error => {
+              console.error('Error fetching student info:', error);
+            });
+        }
+      } catch (error) {
+        console.error('Error checking existing candidate:', error);
+      }
+    } else {
+      nameElement.innerText = '';
+      courseElement.innerText = '';
+    }
+  });
+
 });
+
+function fetchStudentInfo(studentId, position, index) {
+
+  const nameElement = document.getElementById(`${position}_name_${index}`);
+  const courseElement = document.getElementById(`${position}_course_${index}`);
+
+  if (!studentId) {
+    nameElement.innerText = '';
+    courseElement.innerText = '';
+    return;
+  }
+
+  fetch(`/get_student_info/${studentId}`)
+    .then(response => response.json())
+    .then(data => {
+
+      if (data.success) {
+        const studentData = data.data;
+        nameElement.innerText = `Name: ${studentData.name.first} ${studentData.name.last}`;
+        courseElement.innerText = `Course: ${studentData.course}`;
+      } else {
+        nameElement.innerText = '';
+        courseElement.innerText = '';
+        alert(data.message); 
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching student info:', error);
+    });
+
+}
 
 const enteredStudentIds = new Map();
 
-function checkForDuplicates(studentId, position, index) {
+async function checkForDuplicates(studentId, position, index) {
+  try {
 
-  if (enteredStudentIds.has(studentId)) {
-    const existingPositions = enteredStudentIds.get(studentId);
+    const response = await fetch(`/check_duplicate/${studentId}/${position}/${index}`);
+    const data = await response.json();
 
-    if (existingPositions.includes(position)) {
-      alert(`Duplicate student ID is detected for ${position} ${index}.
-      Please change it to avoid errors.`);
-    } 
-    else {
-      alert(`Student ID ${studentId} already exists in another position.
-      Please change it to avoid errors.`);
-    }
-  } 
-  else {
-    
-    fetch(`/get_student_info/${studentId}`)
-      .then(response => response.json())
-      .then(data => {
-        if (!data.success) {
-          alert(`Student ID ${studentId} does not belong to any student.
-          Please enter a valid student ID.`);
-        } 
-        else {
-          enteredStudentIds.set(studentId, [position]);
+    if (data.isDuplicate) {
+      if (data.isExistInOtherPosition) {
+        alert(`Student ID ${studentId} already exists in another position. Please change it to avoid errors.`);
+      } else {
+        alert(`Duplicate student ID is detected for ${position} ${index}. Please change it to avoid errors.`);
+      }
+    } else {
+      
+      const existingEntries = enteredStudentIds.get(studentId);
+
+      if (existingEntries && existingEntries.some(entry => entry.position === position && entry.index !== index)) {
+        alert(`Student ID ${studentId} already exists in the same position for a different index. Please change it to avoid errors.`);
+      } 
+      else if (existingEntries && existingEntries.some(entry => entry.position !== position)) {
+        alert(`Student ID ${studentId} already exists in another position. Please change it to avoid errors.`);
+      } 
+      else {
+        
+        if (!existingEntries) {
+          enteredStudentIds.set(studentId, [{ position, index }]);
+        } else {
+          existingEntries.push({ position, index });
         }
-      })
-      .catch(error => {
-        console.error('Error checking student existence:', error);
-      });
-  }
 
-  fetchStudentInfo(studentId, position, index);
+        fetch(`/get_student_info/${studentId}`)
+          .then(response => response.json())
+          .then(data => {
+            if (!data.success) {
+              alert(`Student ID ${studentId} does not belong to any student. Please enter a valid student ID.`);
+            }
+          })
+          .catch(error => {
+            console.error('Error checking student existence:', error);
+          });
+
+        fetchStudentInfo(studentId, position, index);
+      }
+    }
+  } catch (error) {
+    console.error('Error checking for duplicates:', error);
+  }
 }
+
